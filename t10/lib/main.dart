6963a1b17c2b7cc29d3a2119456cc81bf96c2a55
ddthilindra <ddthilindra@gmail.com>
2022-01-21 01:17:48 +0530

    Stateful (dynamic) & Stateless (static) Widget

--- t10/lib/main.dart
+++ t10/lib/main.dart
@@ -27,7 +27,7 @@ class _homePageState extends State<homePage> {
 
   int count=0;
   void increment(){
-    setState(() { //realtime state change (update screen in real time) redraw the build mehod
+    setState(() { //realtime dynamic state change (update screen in real time) redraw the build mehod
       count += 1;
     print("c "'$count');
     });    
